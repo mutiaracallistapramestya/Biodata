6942c2aaf2499b18e0b719872af38ec8e0a0e00d
Mutiara Callista Pramestya <92364379+mutiaracallistapramestya@users.noreply.github.com>
2021-11-10 17:51:25 -0800

    Add files via upload

---
+++ 2117051013_Mutiara Callista Pramestya_Pretest1.cpp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main () {
+	
+	//----- Mutiara Callista Pramestya-2117051013 -----//
+
+//---string---//
+string Nama;
+string Jurusan;
+string Alamat;
+string NPM;
+
+
+//---input---//
+cout << "Masukkan Nama     :"; 
+getline(cin,Nama); 
+cout << "Masukkan Jurusan  :"; 
+getline(cin,Jurusan); 
+cout << "Masukkan NPM      :"; 
+getline(cin,NPM); 
+cout<< "Masukkan Alamat    :"; 
+getline(cin,Alamat); 
+cout << "                              " << endl;
+
+
+//---output---//
+cout<< "=======Biodata=======" <<endl;
+cout<<"Nama      :" << Nama << endl ;
+cout<<"Jurusan   :"<< Jurusan << endl;
+cout<<"NPM       :" << NPM <<endl;
+cout<<"Alamat    :" << Alamat <<endl;
+cout<< "=====================";
+
+	return 0;
+}
